1952926d7ed0f6c594af770393892c93e324b7f1
andrewluke <aluke4@byu.edu>
2021-01-27 23:43:05 -0700

    * package.json: * package-lock.json: * index.html: * Home.cs: * GradesModel.cs: * Index.cshtml: * _ViewStart.cshtml: * Grades.cshtml: * _ViewImports.cshtml: * _Layout.cshtml: * GradesResult.cshtml:
    * assignment1.csproj: assignment 2 finished

---
+++ assignment1/Controllers/Home.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using assignment1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace assignment1.Controllers
+{
+    public class Home : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Grades")]
+        public IActionResult Grades()
+        {
+            return View();
+        }
+
+        [HttpPost("Grades")]
+        public IActionResult GradesResult(GradesModel model)
+        {
+            ViewBag.assignments = model.assignments;
+            ViewBag.group = model.group;
+            ViewBag.quizzes = model.quizzes;
+            ViewBag.exams = model.exams;
+            ViewBag.intex = model.intex;
+            return View();
+        }
+    }
+}
---
+++ assignment1/Models/GradesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace assignment1.Models
+{
+    public class GradesModel
+    {
+        [Required]
+        [Range(0,100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public double assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public double group { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public double quizzes { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public double exams { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public double intex { get; set; }
+    }
+}
